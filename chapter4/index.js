37120f025f780a7f1c15a7ac3dc7a040321d90c6
Gerd Langer <gerd.langer@famlanger.de>
2025-12-23 12:18:51 +0100

    inline Doku optimiert

--- chapter4/index.js
+++ chapter4/index.js
@@ -11,19 +11,15 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 /* Page Routing for the App = Controller*/
 app.get('/',(req,res)=>{
-    //res.sendFile(path.join(__dirname, 'pages', 'index.html'))
     res.render('index');
 })
 app.get('/about',(req,res)=>{
-    //res.sendFile(path.join(__dirname, 'pages', 'about.html'))
     res.render('about');
 })
 app.get('/contact',(req,res)=>{
-    // res.sendFile(path.join(__dirname, 'pages', 'contact.html'))
     res.render('contact');
 })
 app.get('/post',(req,res)=>{
-    // res.sendFile(path.join(__dirname, 'pages', 'post.html'))
     res.render('post');
 })
 /* Start the server */
