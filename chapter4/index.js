0d97cd3f16a83181e5ba6fe5a675642986a75ed2
Gerd Langer <gerd.langer@famlanger.de>
2025-12-23 11:54:35 +0100

    refactored für Nutzung der Templating Engine EJS

--- chapter4/index.js
+++ chapter4/index.js
@@ -1,22 +1,30 @@
 const express = require('express')
 const path = require('path')
 const app = express()
+const ejs = require('ejs')
+
+/* Set EJS as the templating engine */
+app.set('view engine','ejs')
 
 /* Serving static files */
 app.use(express.static(path.join(__dirname, 'public')))
 
 /* Page Routing for the App = Controller*/
 app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname, 'pages', 'index.html'))
+    //res.sendFile(path.join(__dirname, 'pages', 'index.html'))
+    res.render('index');
 })
 app.get('/about',(req,res)=>{
-    res.sendFile(path.join(__dirname, 'pages', 'about.html'))
+    //res.sendFile(path.join(__dirname, 'pages', 'about.html'))
+    res.render('about');
 })
 app.get('/contact',(req,res)=>{
-    res.sendFile(path.join(__dirname, 'pages', 'contact.html'))
+    // res.sendFile(path.join(__dirname, 'pages', 'contact.html'))
+    res.render('contact');
 })
 app.get('/post',(req,res)=>{
-    res.sendFile(path.join(__dirname, 'pages', 'post.html'))
+    // res.sendFile(path.join(__dirname, 'pages', 'post.html'))
+    res.render('post');
 })
 /* Start the server */
 app.listen(4000, () => {
